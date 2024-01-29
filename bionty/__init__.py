cc86a90daf80366ecc7dceda3979b0b52c9b8dfa
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-01-29 23:49:07 +0800

    🎨 Import orms from lb

--- bionty/__init__.py
+++ bionty/__init__.py
@@ -13,6 +13,44 @@ This is the complete API reference:
    ExampleClass
 """
 
-__version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1rc1
+__version__ = "0.40.0"
 
-from ._core import ExampleClass, example_function
+from lamindb_setup import _check_instance_setup
+from lamindb_setup._check_instance_setup import _INSTANCE_NOT_SETUP_WARNING
+from lnschema_bionty import ids
+
+_INSTANCE_SETUP = _check_instance_setup(from_lamindb=True)
+
+
+class InstanceNotSetupError(Exception):
+    pass
+
+
+def __getattr__(name):
+    raise InstanceNotSetupError(
+        f"{_INSTANCE_NOT_SETUP_WARNING}If you used the CLI to init or load an instance,"
+        " please RESTART the python session (in a notebook, restart kernel)"
+    )
+
+
+# only import all other functionality if setup was successful
+if _INSTANCE_SETUP:
+    del InstanceNotSetupError
+    del __getattr__  # delete so that imports work out
+    from lnschema_bionty import (
+        CellLine,
+        CellMarker,
+        CellType,
+        DevelopmentalStage,
+        Disease,
+        Ethnicity,
+        ExperimentalFactor,
+        Gene,
+        Organism,
+        Pathway,
+        Phenotype,
+        Protein,
+        PublicSource,
+        Tissue,
+    )
+    from lnschema_bionty.dev._settings import settings
