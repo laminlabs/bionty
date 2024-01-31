912c1e1c7aa937300dd9eca1d61a3fccd221b2fe
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-01-31 04:00:17 -0500

    👷 Upload docs (#4)

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,5 @@
 import nox
+from laminci import move_built_docs_to_docs_slash_project_slug
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
 nox.options.default_venv_backend = "none"
@@ -9,10 +10,19 @@ def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
 
+# @nox.session
+# def docs(session: nox.Session):
+#     import lamindb_setup as ln_setup
+
+#     login_testuser1(session)
+#     ln_setup.init(storage="./docsbuild")
+#     build_docs(session)
+
+
 @nox.session()
 def build(session):
     session.run(*"pip install -e .[dev]".split())
-    session.run(*"pip install git+https://github.com/laminlabs/lamindb-setup".split())
-    login_testuser1(session)
+    # session.run(*"pip install git+https://github.com/laminlabs/lamindb".split())
     # run_pytest(session, coverage=False)
-    # build_docs(session, strict=True)
+    # docs(session)
+    # move_built_docs_to_docs_slash_project_slug()
