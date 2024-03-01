d932dca76a9e2cbb7db678d388c11a7bcb939c9a
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-03-01 13:53:26 +0100

    Release 0.41.0

--- bionty/__init__.py
+++ bionty/__init__.py
@@ -123,7 +123,7 @@ Developer API:
    dev
 """
 
-__version__ = "0.40.3"
+__version__ = "0.41.0"
 
 from lamindb_setup import _check_instance_setup
 from lamindb_setup._check_instance_setup import _INSTANCE_NOT_SETUP_WARNING
