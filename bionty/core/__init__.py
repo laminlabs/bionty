ba89eb0be6c816afd3f557908f699f9d19ce795a
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-03-14 03:16:24 +0800

    🚚 Rename dev to core (#13)

---
+++ bionty/core/__init__.py
@@ -0,0 +1,4 @@
+import lnschema_bionty as _lnschema_bionty
+from lnschema_bionty.core import *  # noqa
+
+__doc__ = _lnschema_bionty.core.__doc__.replace("lnschema_bionty", "bionty")
--- bionty/dev/__init__.py
+++
@@ -1,4 +0,0 @@
-import lnschema_bionty as _lnschema_bionty
-from lnschema_bionty.dev import *  # noqa
-
-__doc__ = _lnschema_bionty.dev.__doc__.replace("lnschema_bionty", "bionty")
