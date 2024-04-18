f071d9e75c50de7002cf8d8176ca70343e304b13
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-04-18 11:08:02 +0200

    🔖 Release 0.42.9

--- bionty/__init__.py
+++ bionty/__init__.py
@@ -122,7 +122,7 @@ Developer API:
    core
 """
 
-__version__ = "0.42.8"
+__version__ = "0.42.9"
 
 from lamindb_setup._check_setup import InstanceNotSetupError, _check_instance_setup
 from lnschema_bionty import ids
