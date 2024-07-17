0475e8e3199866f2211d6987f54a42e1ec3c6b2f
Alex Wolf <f.alexander.wolf@gmail.com>
2024-07-17 10:19:01 +0200

    🔖 Release 0.44.1

--- bionty/__init__.py
+++ bionty/__init__.py
@@ -122,7 +122,7 @@ Developer API:
    core
 """
 
-__version__ = "0.44.0"
+__version__ = "0.44.1"
 
 from lamindb_setup._check_setup import InstanceNotSetupError, _check_instance_setup
 from lnschema_bionty import ids
