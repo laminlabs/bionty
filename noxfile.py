f18ba09a757c981bebd52f06dab445e4676b2d4d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-07-29 18:55:36 +0200

    💚 Fix docs

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,5 @@
 import nox
-from laminci import move_built_docs_to_docs_slash_project_slug
-from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+from laminci.nox import build_docs, run, run_pre_commit
 
 nox.options.default_venv_backend = "none"
 
@@ -8,11 +7,6 @@ nox.options.default_venv_backend = "none"
 @nox.session
 def lint(session: nox.Session) -> None:
     run_pre_commit(session)
-
-
-# @nox.session
-# def docs(session: nox.Session):
-#     build_docs(session, strict=True)
 
 
 @nox.session
@@ -27,4 +21,5 @@ def build(session: nox.Session, group: str):
         session.run(*f"pytest {coverage_args} ./tests".split())
     elif group == "bionty-docs":
         session.run(*f"pytest -s {coverage_args} ./docs/guide".split())
+        run(session, "lamin init --storage ./docsbuild --schema bionty")
         build_docs(session, strict=True)
