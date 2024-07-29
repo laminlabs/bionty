b64945a38302a09ad0bbf7ad9cd8ddc5938a0da6
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-07-29 19:05:34 +0200

    💚 Install lamindb-setup from branch

--- noxfile.py
+++ noxfile.py
@@ -14,7 +14,9 @@ def lint(session: nox.Session) -> None:
 def build(session: nox.Session, group: str):
     session.run(*"uv pip install --system -e .[dev]".split())
     # session.run(*"pip install git+https://github.com/laminlabs/lamindb".split())
-    # session.run(*"pip install git+https://github.com/laminlabs/lamindb-setup".split())
+    session.run(
+        *"pip install git+https://github.com/laminlabs/lamindb-setup@single-bionty-repo".split()
+    )
     # session.run(*"pip install git+https://github.com/laminlabs/lnschema-core".split())
     coverage_args = "--cov=bionty --cov-append --cov-report=term-missing"
     if group == "bionty-unit":
