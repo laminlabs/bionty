19a351134136496bcb0621611e7d87f4846c8b61
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-07-31 13:50:54 +0200

    🎨 Make CellMkarker.name unique together with organism

--- bionty/core/_bionty.py
+++ bionty/core/_bionty.py
@@ -1,4 +1,8 @@
+from typing import Type
+
+import pandas as pd
 from lamin_utils import logger
+from lnschema_core.models import Record
 
 import bionty.base as bionty_base
 
@@ -52,3 +56,49 @@ def set_latest_sources_as_currently_used():
         records.filter(entity=entity, organism=organism).exclude(uid=latest_uid).update(
             currently_used=False
         )
+
+
+def filter_bionty_df_columns(
+    model: Type[Record], public_ontology: bionty_base.PublicOntology
+) -> pd.DataFrame:
+    """Filter columns of public ontology to match the model fields."""
+    bionty_df = pd.DataFrame()
+    if public_ontology is not None:
+        model_field_names = {i.name for i in model._meta.fields}
+        # parents needs to be added here as relationships aren't in fields
+        model_field_names.add("parents")
+        bionty_df = _prepare_bionty_df(model, public_ontology.df().reset_index())
+        bionty_df = bionty_df.loc[:, bionty_df.columns.isin(model_field_names)]
+    return bionty_df
+
+
+def _prepare_bionty_df(model: type[Record], bionty_df: pd.DataFrame):
+    """Prepare the bionty DataFrame to match the model fields."""
+    if model.__get_name_with_schema__() == "bionty.Gene":
+        # groupby ensembl_gene_id and concat ncbi_gene_ids
+        groupby_id_col = (
+            "ensembl_gene_id" if "ensembl_gene_id" in bionty_df else "stable_id"
+        )
+        bionty_df.drop(
+            columns=["hgnc_id", "mgi_id", "index"], errors="ignore", inplace=True
+        )
+        bionty_df.drop_duplicates([groupby_id_col, "ncbi_gene_id"], inplace=True)
+        bionty_df["ncbi_gene_id"] = bionty_df["ncbi_gene_id"].fillna("")
+        bionty_df = (
+            bionty_df.groupby(groupby_id_col)
+            .agg(
+                {
+                    "symbol": "first",
+                    "ncbi_gene_id": "|".join,
+                    "biotype": "first",
+                    "description": "first",
+                    "synonyms": "first",
+                }
+            )
+            .reset_index()
+        )
+        bionty_df.rename(columns={"ncbi_gene_id": "ncbi_gene_ids"}, inplace=True)
+
+    # rename definition to description for the bionty registry in db
+    bionty_df.rename(columns={"definition": "description"}, inplace=True)
+    return bionty_df
