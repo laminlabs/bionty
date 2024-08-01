04af6c928ad48332ed142e652980cb2232c3adfc
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-08-01 21:38:28 +0200

    🚚 Rename `.sources()` to `.list_source()` (#27)

--- bionty/models.py
+++ bionty/models.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, List, Tuple, overload
+from typing import TYPE_CHECKING, List, Optional, Tuple, overload
 
 import numpy as np
 from django.db import models
@@ -34,7 +34,7 @@ class StaticReference:
         self._source = source_record
 
     def df(self) -> DataFrame:
-        return self._source.df.load()
+        return self._source.dataframe_artifact.load()
 
 
 class BioRecord(Record, HasParents, CanValidate):
@@ -115,19 +115,28 @@ class BioRecord(Record, HasParents, CanValidate):
         super().__init__(*args, **kwargs)
 
     @classmethod
-    def sources(cls, currently_used: bool = None) -> Source:
+    def list_source(
+        cls,
+        currently_used: bool | None = None,
+        in_db: bool | None = None,
+        organism: str | None = None,
+    ) -> Source:
         """Default source for the registry.
 
         Args:
             currently_used: Only returns currently used sources
 
         Examples:
-            >>> bionty.Gene.sources()
-            >>> bionty.Gene.sources(currently_used=True)
+            >>> bionty.Gene.list_source()
+            >>> bionty.Gene.list_source(currently_used=True)
         """
         filters = {}
         if currently_used is not None:
             filters["currently_used"] = currently_used
+        if in_db is not None:
+            filters["in_db"] = in_db
+        if organism is not None:
+            filters["organism"] = organism  # type:ignore
         return Source.filter(entity=cls.__get_name_with_schema__(), **filters)
 
     @classmethod
