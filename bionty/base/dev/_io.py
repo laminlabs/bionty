1c817fe272a7103609a45f89a725ae6fb34d3288
zethson <lukas.heumos@posteo.net>
2024-08-14 15:01:27 +0200

    :art: Refactor
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- bionty/base/dev/_io.py
+++ bionty/base/dev/_io.py
@@ -110,7 +110,8 @@ def s3_bionty_assets(
 
     try:
         s3_object = s3_client.get_object(Bucket=bucket, Key=filename)
-    except s3_client.exceptions.ClientError:
+    except s3_client.exceptions.ClientError as e:
+        print(e)
         return localpath
 
     cloud_mts = s3_object["LastModified"].timestamp()
