8089222e2a558a62e2ce60952052412e8de42ea6
Alex Wolf <f.alexander.wolf@gmail.com>
2024-09-29 22:34:19 +0200

    🏗️ Load bionty sources here instead of in ln.connect

--- bionty/__init__.py
+++ bionty/__init__.py
@@ -147,6 +147,10 @@ Bionty base:
 
 __version__ = "0.50.2"
 
+from lamindb_setup.core._setup_bionty_sources import (
+    load_bionty_sources as _load_bionty_sources,
+)
+
 from . import base, ids
 
 base.sync_sources()
@@ -155,13 +159,16 @@ from lamindb_setup._check_setup import _check_instance_setup
 
 
 def __getattr__(name):
-    if name not in {"models"}:
-        _check_instance_setup(from_lamindb=True, from_module="bionty")
+    if name != "models":
+        _check_instance_setup(from_module="bionty")
     return globals()[name]
 
 
 if _check_instance_setup():
-    import lamindb  # this is needed as even the Record base class is defined in lamindb
+    import lamindb as _lamindb  # this is needed as even the Record base class is defined in lamindb
+    from lamindb_setup import settings as _setup_settings
+
+    _load_bionty_sources(_setup_settings.instance)
 
     del __getattr__  # delete so that imports work out
     from .core._settings import settings
