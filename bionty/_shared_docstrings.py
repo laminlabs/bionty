e9d8e5f2c7c212987b7f8447426a38274f101c7d
Lukas Heumos <lukas.heumos@posteo.net>
2025-02-10 12:38:05 +0100

    🎨 Revert
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- bionty/_shared_docstrings.py
+++ bionty/_shared_docstrings.py
@@ -1,8 +1,4 @@
 doc_from_source = """\
-df: DataFrame with a column of identifiers
-        column: If `column` is `None`, checks the existing index for compliance with
-                  the default identifier.
-                If `column` denotes an entity identifier, tries to map that identifier
-                  to the default identifier.
-        field: The type of identifier for mapping.
+source: Optional Source record to use
+        mute: Whether to suppress logging
 """
