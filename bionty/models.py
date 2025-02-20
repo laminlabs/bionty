16fd0a7b6dc2c879bb10b932c226238365318f4c
Lukas Heumos <lukas.heumos@posteo.net>
2025-02-20 14:10:25 +0100

    🎨 Locals
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- bionty/models.py
+++ bionty/models.py
@@ -582,7 +582,7 @@ class Organism(BioRecord, TracksRun, TracksUpdates):
             >>> record = Organism.from_source(name="human")
             >>> record = Organism.from_source(ontology_id="9606")
         """
-        return super().from_source(**locals())
+        return super().from_source(**{k: v for k, v in locals().items() if k != "cls"})
 
 
 class Gene(BioRecord, TracksRun, TracksUpdates):
