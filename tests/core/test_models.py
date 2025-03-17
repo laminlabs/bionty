7c831610ff7566a7018b386ea18561550b027d0f
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-03-17 14:12:35 +0100

    🧪 Move test_bionty_encode_uid from lamindb here

--- tests/core/test_models.py
+++ tests/core/test_models.py
@@ -18,19 +18,48 @@ def test_add_source(setup_instance):
     assert new_source.dataframe_artifact is not None
 
 
-def test_source_uids(setup_instance):
-    disease_ontology = bt.Source(
+def test_encode_uids(setup_instance):
+    gene = bt.Gene(
+        ensembl_gene_id="ENSG00000081059",
+        symbol="TCF7",
+        _skip_validation=True,
+    )
+    assert gene.uid == "7IkHKPl0ScQR"
+
+    cell_type = bt.CellType(
+        ontology_id="CL:0000084",
+        _skip_validation=True,
+    )
+    assert cell_type.uid == "22LvKd01"
+
+    organism = bt.Organism(
+        ontology_id="NCBITaxon:9606",
+        name="human",
+        _skip_validation=True,
+    )
+    assert organism.uid == "1dpCL6Td"
+
+    bt.settings.organism = "human"
+    cell_marker = bt.CellMarker(
+        name="test",
+        organism=bt.settings.organism,
+        _skip_validation=True,
+    )
+    assert cell_marker.uid == "2dZ52W9noUDK"
+
+    disease = bt.Source(
         entity="bionty.Disease",
         name="mondo",
         version="2023-04-04",
         _skip_validation=True,
     )
-    assert disease_ontology.uid == "Hgw08Vk3"
-    phenotype_ontology = bt.Source(
+    assert disease.uid == "Hgw08Vk3"
+
+    phenotype = bt.Source(
         entity="bionty.Phenotype",
         name="hp",
         version="2023-06-17",
         organism="human",
         _skip_validation=True,
     )
-    assert phenotype_ontology.uid == "451W7iJS"
+    assert phenotype.uid == "451W7iJS"
