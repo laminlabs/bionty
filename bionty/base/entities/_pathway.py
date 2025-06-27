30b1fc50f83e032db25ae7fe5aff4d1c47767e35
namsaraeva <a.namsaraeva@tum.de>
2025-06-27 18:23:20 +0200

    ✨ Enhance entity classes with overloaded constructors for better type safety

--- bionty/base/entities/_pathway.py
+++ bionty/base/entities/_pathway.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
 
-from typing import Literal
+from typing import Literal, overload
 
 from bionty.base._public_ontology import PublicOntology
 from bionty.base.dev._doc_util import _doc_params
 
 from ._shared_docstrings import doc_entites
+
+GOVersions = Literal[
+    "2024-11-03",
+    "2024-06-17",
+    "2023-05-10",
+]
+
+PWVersions = Literal["7.84"]
 
 
 @_doc_params(doc_entities=doc_entites)
@@ -22,17 +30,29 @@ class Pathway(PublicOntology):
         {doc_entities}
     """
 
+    @overload
+    def __init__(
+        self,
+        organism: Literal["all"] | None = None,
+        source: Literal["go"] = None,
+        version: GOVersions | None = None,
+        **kwargs,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self,
+        organism: Literal["all"] | None = None,
+        source: Literal["pw"] = None,
+        version: PWVersions | None = None,
+        **kwargs,
+    ) -> None: ...
+
     def __init__(
         self,
         organism: Literal["all"] | None = None,
         source: Literal["go", "pw"] | None = None,
-        version: Literal[
-            "2024-11-03",
-            "2024-06-17",
-            "2023-05-10",
-            "7.84",
-        ]
-        | None = None,
+        version: GOVersions | PWVersions | None = None,
         **kwargs,
     ) -> None:
         super().__init__(source=source, version=version, organism=organism, **kwargs)
--- bionty/base/entities/_phenotype.py
+++ bionty/base/entities/_phenotype.py
@@ -1,11 +1,34 @@
 from __future__ import annotations
 
-from typing import Literal
+from typing import Literal, overload
 
 from bionty.base._public_ontology import PublicOntology
 from bionty.base.dev._doc_util import _doc_params
 
 from ._shared_docstrings import doc_entites
+
+HPVersions = Literal[
+    "2025-05-06",
+    "2024-04-26",
+    "2024-03-06",
+    "2023-06-17",
+    "2023-04-05",
+    "2023-01-27",
+]
+
+PHEVersions = Literal["1.2"]
+
+PATOVersions = Literal[
+    "2025-05-14",
+    "2024-06-18",
+    "2024-04-18",
+    "2024-03-28",
+    "2024-02-07",
+    "2024-01-22",
+    "2023-05-31",
+    "2023-05-18",
+    "2022-12-17",
+]
 
 
 @_doc_params(doc_entities=doc_entites)
@@ -22,29 +45,38 @@ class Phenotype(PublicOntology):
         {doc_entities}
     """
 
+    @overload
+    def __init__(
+        self,
+        organism: Literal["human"] | None = None,
+        source: Literal["hp"] = None,
+        version: HPVersions | None = None,
+        **kwargs,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self,
+        organism: Literal["human"] | None = None,
+        source: Literal["phe"] = None,
+        version: PHEVersions | None = None,
+        **kwargs,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self,
+        organism: Literal["all"] | None = None,
+        source: Literal["pato"] = None,
+        version: PATOVersions | None = None,
+        **kwargs,
+    ) -> None: ...
+
     def __init__(
         self,
         organism: Literal["human", "all"] | None = None,
         source: Literal["hp", "phe", "pato"] | None = None,
-        version: Literal[
-            "2025-05-14",
-            "2025-05-06",
-            "2024-06-18",
-            "2024-04-26",
-            "2024-04-18",
-            "2024-03-28",
-            "2024-03-06",
-            "2024-02-07",
-            "2024-01-22",
-            "2023-06-17",
-            "2023-05-31",
-            "2023-05-18",
-            "2023-04-05",
-            "2023-01-27",
-            "2022-12-17",
-            "1.2",
-        ]
-        | None = None,
+        version: HPVersions | PHEVersions | PATOVersions | None = None,
         **kwargs,
     ) -> None:
         super().__init__(
