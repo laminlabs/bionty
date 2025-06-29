e095dd65623361aa9c65d3afe4bedcb94a413f4f
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-06-29 12:22:37 +0200

    🎨 Better ux

--- tests/core/test_source.py
+++ tests/core/test_source.py
@@ -240,12 +240,10 @@ def test_import_source_update_records():
 
 
 def test_import_source_no_prefix_filter():
-    from bionty.base._ontology import Ontology
-
     efo_public = bt.base.ExperimentalFactor(version="3.78.0")
-    efo_ontology = Ontology(bt.base.settings.dynamicdir / efo_public._ontology_filename)
-    efo_ontology_df = efo_ontology.to_df()
-    # efo_ontology_df.shape()
+    efo_ontology_df = efo_public.to_pronto().to_df(include_id_prefixes=None)
+    assert efo_ontology_df.shape[0] == 66971
+    assert efo_ontology_df.index.str.startswith("EFO:").sum() == 18229
     source = bt.ExperimentalFactor.add_source(source="efo", df=efo_ontology_df)
     bt.ExperimentalFactor.import_source(source=source)
     assert bt.ExperimentalFactor.filter().count() == 66971
