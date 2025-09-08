b932c1ad3cadd0c7d2cd501b7d09b48a0485ef8f
Alex Wolf <f.alexander.wolf@gmail.com>
2025-09-08 09:08:24 +0200

    ♻️ Import the whole public API

--- bionty/__init__.py
+++ bionty/__init__.py
@@ -95,29 +95,22 @@ from lamindb_setup._check_setup import _check_instance_setup
 
 from . import _biorecord, base, ids
 
+_check_instance_setup(from_module="bionty")
 
-def __getattr__(name):
-    if name != "models":
-        _check_instance_setup(from_module="bionty")
-    return globals()[name]
-
-
-if _check_instance_setup():
-    del __getattr__  # delete so that imports work out
-    from .core._settings import settings
-    from .models import (
-        CellLine,
-        CellMarker,
-        CellType,
-        DevelopmentalStage,
-        Disease,
-        Ethnicity,
-        ExperimentalFactor,
-        Gene,
-        Organism,
-        Pathway,
-        Phenotype,
-        Protein,
-        Source,
-        Tissue,
-    )
+from .core._settings import settings
+from .models import (
+    CellLine,
+    CellMarker,
+    CellType,
+    DevelopmentalStage,
+    Disease,
+    Ethnicity,
+    ExperimentalFactor,
+    Gene,
+    Organism,
+    Pathway,
+    Phenotype,
+    Protein,
+    Source,
+    Tissue,
+)
