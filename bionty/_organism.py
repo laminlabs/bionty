8e5bde07fe908d8133f33c8c0240039035c7443d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-12-08 16:55:16 +0100

    ♻️ Refactor organism get and create

--- bionty/_organism.py
+++ bionty/_organism.py
@@ -16,43 +16,30 @@ class OrganismNotSet(SystemExit):
 def create_or_get_organism_record(
     organism: str | Organism | None, registry: type[BioRecord], field: str | None = None
 ) -> Organism | None:
-    """Create or get an organism record from the given organism name."""
-    # return None if a registry doesn't require organism field
+    """Create or get an organism record.
+
+    From the following options:
+    1. the globally setting of organism
+    2. the passed organism record
+    3. the passed organism name
+    """
+    # also returns None if a registry doesn't require organism field
     organism_record = None
     if is_organism_required(registry):
-        # using global setting of organism
         from .core._settings import settings
         from .models import Organism
 
+        # using global setting of organism
         if organism is None and settings.organism is not None:
-            logger.debug(f"using default organism = {settings.organism.name}")
+            logger.debug(f"using configured organism = {settings.organism.name}")
             return settings.organism
 
+        # create or get organism record
         if isinstance(organism, Organism):
             organism_record = organism
         elif isinstance(organism, str):
-            try:
-                # existing organism record
-                organism_record = Organism.objects.get(name=organism)
-            except ObjectDoesNotExist:
-                try:
-                    # create a organism record from bionty reference
-                    organisms = Organism.from_values([organism])
-                    if len(organisms) == 0:
-                        raise ValueError(
-                            f"Organism {organism} can't be created from the bionty reference, check your spelling or create it manually."
-                        )
-                    organism_record = organisms[0].save()  # type:ignore
-                except KeyError:
-                    # no such organism is found in bionty reference
-                    organism_record = None
+            organism_record = get_or_create_organism_from_name(name=organism)
 
-        if organism_record is None:
-            if hasattr(registry, "_ontology_id_field") and field in {
-                registry._ontology_id_field,
-                "uid",
-            }:
-                return None
     return organism_record
 
 
@@ -73,7 +60,31 @@ def is_organism_required(registry: type[BioRecord]) -> bool:
         return False
 
 
-def organism_from_ensembl_id(id: str, using_key: str | None) -> Organism | None:
+def get_or_create_organism_from_name(
+    name: str, using_key: str | None = None
+) -> Organism:
+    """Create organism record if not exists."""
+    import bionty as bt
+
+    using_key = None if using_key == "default" else using_key
+
+    organism_record = bt.Organism.connect(using_key).filter(name=name).one_or_none()
+    if organism_record is None:
+        from lamindb.errors import DoesNotExist
+
+        try:
+            organism_record = bt.Organism.from_source(
+                name=name
+            )  # here assumes the default source is ensembl vertebrates
+            organism_record.save(using=using_key)
+        except DoesNotExist:
+            raise OrganismNotSet(
+                f"Organism {name} can't be created from the source, check your spelling or create it manually."
+            ) from None
+    return organism_record
+
+
+def organism_from_ensembl_id(id: str, using_key: str | None = None) -> Organism | None:
     """Get organism record from ensembl id."""
     import bionty as bt
     from bionty.base.dev._io import s3_bionty_assets
@@ -88,28 +99,9 @@ def organism_from_ensembl_id(id: str, using_key: str | None) -> Organism | None:
     prefix = (
         re.search(r"^[A-Za-z]+", id).group(0) if re.search(r"^[A-Za-z]+", id) else id
     )
-    print("prefix:", prefix)
+
+    # for ensembl vertebrates, we infer organism from the ensembl prefix
     if prefix in ensembl_prefixes.index:
         organism_name = ensembl_prefixes.loc[prefix, "name"].lower()
-
-        using_key = None if using_key == "default" else using_key
-
-        organism_record = (
-            bt.Organism.connect(using_key).filter(name=organism_name).one_or_none()
-        )
-        if organism_record is None:
-            organisms = bt.Organism.from_values(
-                [organism_name],
-                source=bt.Source.filter(
-                    entity="bionty.Organism", currently_used=True
-                ).first(),
-            )
-            if len(organisms) > 0:
-                organism_record = organisms[0]
-                organism_record.save(using=using_key)
-            else:
-                raise OrganismNotSet(
-                    f"Organism {organism_name} can't be created from the source, check your spelling or create it manually."
-                )
-        return organism_record
+        return get_or_create_organism_from_name(name=organism_name, using_key=using_key)
     return None
