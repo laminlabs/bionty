8976694695382e01ff044b86e86694f9a04aa811
Lukas Heumos <lukas.heumos@posteo.net>
2026-01-08 11:18:37 +0100

    ✨ Add ensembl 114
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- bionty/base/entities/_gene.py
+++ bionty/base/entities/_gene.py
@@ -53,6 +53,7 @@ class Gene(PublicOntology):
         organism: Literal["human", "mouse", "saccharomyces cerevisiae"] | None = None,
         source: Literal["ensembl"] | None = None,
         version: Literal[
+            "release-114",
             "release-112",
             "release-111",
             "release-110",
