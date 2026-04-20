332bebeb42b3f39a50653d363b58dac328aa5ba2
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2026-04-20 14:30:47 +0200

    💚 Warn if multiple organisms match name

--- bionty/_organism.py
+++ bionty/_organism.py
@@ -64,11 +64,17 @@ def get_or_create_organism_from_name(
         )
         if organism_record is None:
             from lamindb.errors import DoesNotExist
+            from lamindb.models.query_set import SQLRecordList
 
             # try to create from ncbitaxon source
             try:
                 source = bt.Source.filter(name="ncbitaxon").first()
                 organism_record = bt.Organism.from_source(name, source=source)
+                if isinstance(organism_record, SQLRecordList):
+                    organism_record = organism_record[0]
+                    logger.warning(
+                        f"Multiple organisms found for {name}, saving the first one: {organism_record}"
+                    )
                 organism_record.save(using=using_key)
             except DoesNotExist:
                 if error:
